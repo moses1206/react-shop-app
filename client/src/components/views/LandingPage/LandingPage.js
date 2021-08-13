cdc9c9306f5e4341bba8fa817d3629feeed94e90
unknown <mose233@gmail.com>
2021-08-13 21:55:45 +0900

    Good

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,16 +1,19 @@
-import React from 'react'
-import { FaCode } from "react-icons/fa";
+import React, { useEffect } from 'react';
+import Axios from 'axios';
+import { FaCode } from 'react-icons/fa';
 
 function LandingPage() {
-    return (
-        <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
-            </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
-    )
+  useEffect(() => {
+    Axios.post('/api/product/getproducts').then((response) => {
+      if (response.data.success) {
+        console.log(response.data);
+      } else {
+        alert('상품들을 가져오는 것을 실패하였습니다. !!');
+      }
+    });
+  }, []);
+
+  return <div>LandingPage</div>;
 }
 
-export default LandingPage
+export default LandingPage;
--- server/routes/product.js
+++ server/routes/product.js
@@ -46,4 +46,15 @@ router.post('/', auth, (req, res) => {
   });
 });
 
+router.post('/getproducts', auth, (req, res) => {
+  // product collection의 모든 상품정보를 가져오기
+  Product.find()
+    .populate('writer')
+    .exec((err, productsInfo) => {
+      if (err) return res.status(400).json({ success: false, err });
+
+      return res.status(200).json({ success: true, productsInfo });
+    });
+});
+
 module.exports = router;
